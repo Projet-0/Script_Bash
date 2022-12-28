73cf8e189f75acb4a8e532c39a74660ebcbdc748
Projet-0 <98283760+Projet-0@users.noreply.github.com>
2022-12-28 19:07:09 +0100

    Add files via upload
    Almost last version of the project. All options are good except "-a" that corresponds to display hidden files. I can always display hidden files but, when -a is disabled I need to exclude them. I haven't found now a regex expression to exclude them properly.

---
+++ dirstat.sh
@@ -0,0 +1,130 @@
+#!/bin/bash
+
+
+path='/home/mohamed/Bureau/'
+file_path='/home/mohamed/Bureau/SFTP/README.md'
+
+parameter () {
+
+	path='Variable initiale a modifier en fonction des boucles' ;
+	#echo $@ ; # $@ EST LA VALEUR DE TOUT CE QUI EST PASSE EN ARGUMENT
+	local="no value";
+	taille="--max-depth=1"
+	global="no value for"
+	#exclude="./.*"
+	# TEST de exclude
+	exclude="[\/]+[.]+"
+
+	while getopts "d:fahsr:o:" OPT; do
+
+	# Je récupère le texte associé à l'option
+	case "${OPT}" in
+	d) 
+	    d_p=${OPTARG}; # dossier a cibler
+	    local=`du $d_p $taille`
+
+            ;;	
+	h)
+	    # h_p=${OPTARG}; # Lisible en Ko,Mo
+	    h_p="-h";
+	    local=`du $d_p $h_p $taille`
+            ;;	
+        s)
+            #s_v=${OPTARG} # Tri par ordre decroissant
+	    s_p="sort -hr "  
+	    local=`du $d_p $h_p $taille | sort -nr `
+	    ;;
+	
+	r)
+            r_p="${OPTARG}" # utilise un regex
+	    echo "$r_p" 
+	    echo "je viens de mettre la valeur du regex"
+	    ;;
+	f)
+            f_p="-a";; # On cible egalement les fichiers
+	a)
+            #a_v=${OPTARG}
+	    exclude="" ;  
+	    ;; # affiche les fichiers/dossiers caches
+	o)
+            #o_p=${OPTARG};; # ecrit dans un fichier
+	#TEST 
+	    o_p=${OPTARG}	
+	    echo "$o_p"
+	    ;;
+        *)
+            fourth="Error: ${OPT} is not acceptable";; # argument pas acceptanel
+
+    	esac
+	done
+
+
+	## IL faut tout faire a la fin comme ca je peux faire une grande commande
+
+	# JE VAIS METTRE LE REGEX DEDANS TOUT SIMPLEMENT	
+
+	#SORT AND
+	if [[ $@ == *"-s"* ]]; 
+	then
+	#ON CHECK LE REGEX 
+	  if [[ $@ == *"-r"* ]]; 
+	  then
+	    #COMMANDE A METTRE
+	    global=`du $h_p $f_p $d_p $taille | $s_p | grep $r_p` # ON CONTIENT LE REGEX ON ENLEVE
+	  else 
+	    #COMMANDE A METTRE
+	    global=`du $h_p $f_p $d_p $taille | $s_p` # PAS DE REGEX
+	  fi
+
+	  #global=`du $h_p $d_p $taille | $s_p ` ## COMMANDE DE DEPART
+
+	## PAS DE SORT PAS BESOIN DE TRIER
+	else 
+	  if [[ $@ == *"-r"* ]]; 
+	  then
+	    #COMMANDE A METTRE
+	    global=`du $h_p $f_p $d_p $taille | grep $r_p` # ON CONTIENT LE REGEX
+	  else 
+	    #COMMANDE A METTRE
+	    global=`du $h_p $f_p $d_p $taille` # PAS DE REGEX
+	  fi
+	  #global=`du $h_p $d_p $taille ` ## COMMANDE DE DEPART
+	fi
+
+
+	#REGEX GREP 
+	#if [[ $@ == *"-r"* ]]; 
+	#then
+	#  global=`$global | grep $r_p`
+	#fi
+
+
+
+	#SAVE
+	
+	if [[ $@ == *"-o"* ]]; 
+	then
+	  echo "$global" > $o_p ;
+	  # cat $o_p ;
+	fi
+
+
+
+	#global=`du $h_p $d_p $taille | $s_p`
+
+	echo "value global"
+	echo "$global"
+	echo "end global"
+
+	
+	echo "Value of d  is : $d_p, Value of d_result is :"
+	echo "value of local is"
+	#echo "$local"
+
+
+}
+
+echo $@ ;
+parameter $@ ;
+
+
